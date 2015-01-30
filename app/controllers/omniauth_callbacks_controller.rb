880a3ced644c56ee90e04b83e1daeb50e0ebd016
Robert Pavlovskyi <robert.pavlovsky@gmail.com>
2015-01-30 11:57:33 +0200

    fixes to athentification

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
 def facebook
-     @user = User.from_omniauth(request.env["omniauth.auth"], current_user)
+     @user = User.from_omniauth(request.env["omniauth.auth"])
      if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
@@ -10,5 +10,5 @@ def facebook
       redirect_to new_user_registration_url
     end
   end
-  
+
 end
